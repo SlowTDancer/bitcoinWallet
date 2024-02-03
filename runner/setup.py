c8b12cd02ceb54d83e61043cd618701807cffbd4
Tsiqara <mtsik21@freeuni.edu.ge>
2024-02-03 17:19:05 +0400

    include routers in setup.py

--- runner/setup.py
+++ runner/setup.py
@@ -2,6 +2,9 @@ import os
 
 from fastapi import FastAPI
 
+from infra.fastapi.transactions import transaction_api
+from infra.fastapi.users import user_api
+from infra.fastapi.wallets import wallet_api
 from infra.in_memory.transaction_in_memory import TransactionInMemory
 from infra.in_memory.user_in_memory import UserInMemory
 from infra.in_memory.wallet_in_memory import WalletInMemory
@@ -9,7 +12,9 @@ from infra.in_memory.wallet_in_memory import WalletInMemory
 
 def init_app() -> FastAPI:
     app = FastAPI()
-    app.include_router()
+    app.include_router(user_api)
+    app.include_router(wallet_api)
+    app.include_router(transaction_api)
 
     # comment line below when you are using test-mode
     # os.environ["REPOSITORY_KIND"] = "sqlite"
