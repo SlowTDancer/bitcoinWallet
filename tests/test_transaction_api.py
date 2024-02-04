f04a4eb55cc18e8c0a1be4ae43de171f3c8debbe
SlowTDancer <ikhut21@freeuni.edu.ge>
2024-02-04 23:46:21 +0400

    black/isort

--- tests/test_transaction_api.py
+++ tests/test_transaction_api.py
@@ -24,15 +24,23 @@ def test_make_transaction_success(client: TestClient) -> None:
     public_key2 = wallet_response2.json()["wallet"]["public_key"]
 
     request_data = {"from_key": public_key1, "to_key": public_key2, "amount": 1}
-    response = client.post("/transactions", json=request_data, headers=user_response.json()["user"])
+    response = client.post(
+        "/transactions", json=request_data, headers=user_response.json()["user"]
+    )
 
     assert response.status_code == 201
     assert "transaction" in response.json()
 
 
 def test_make_transaction_user_does_not_exist(client: TestClient) -> None:
-    request_data = {"from_key": str(uuid.uuid4()), "to_key": str(uuid.uuid4()), "amount": 100}
-    response = client.post("/transactions", json=request_data, headers={"api_key": str(uuid.uuid4())})
+    request_data = {
+        "from_key": str(uuid.uuid4()),
+        "to_key": str(uuid.uuid4()),
+        "amount": 100,
+    }
+    response = client.post(
+        "/transactions", json=request_data, headers={"api_key": str(uuid.uuid4())}
+    )
 
     assert response.status_code == 404
     assert response.json()["error"]["message"] == "User does not exist."
@@ -47,10 +55,15 @@ def test_make_transaction_wallet_does_not_exist(client: TestClient) -> None:
     wallet_address = str(uuid.uuid4())
 
     request_data = {"from_key": wallet_address, "to_key": wallet_address, "amount": 100}
-    response = client.post("/transactions", json=request_data, headers=user_response.json()["user"])
+    response = client.post(
+        "/transactions", json=request_data, headers=user_response.json()["user"]
+    )
 
     assert response.status_code == 405
-    assert response.json()["error"]["message"] == f"Wallet with address <{wallet_address}> does not exist."
+    assert (
+        response.json()["error"]["message"]
+        == f"Wallet with address <{wallet_address}> does not exist."
+    )
 
 
 def test_make_transaction_same_wallets(client: TestClient) -> None:
@@ -64,12 +77,15 @@ def test_make_transaction_same_wallets(client: TestClient) -> None:
     public_key = wallet_response.json()["wallet"]["public_key"]
 
     request_data = {"from_key": public_key, "to_key": public_key, "amount": 1}
-    response = client.post("/transactions", json=request_data, headers=user_response.json()["user"])
+    response = client.post(
+        "/transactions", json=request_data, headers=user_response.json()["user"]
+    )
 
     assert response.status_code == 420
-    assert response.json()["error"][
-               "message"] == (f"You are trying to make transaction from wallet with address <{public_key}> to "
-                              f"same wallet with address <{public_key}>.")
+    assert response.json()["error"]["message"] == (
+        f"You are trying to make transaction from wallet with address <{public_key}> to "
+        f"same wallet with address <{public_key}>."
+    )
 
 
 def test_make_transaction_not_enough_balance(client: TestClient) -> None:
@@ -85,10 +101,15 @@ def test_make_transaction_not_enough_balance(client: TestClient) -> None:
     public_key2 = wallet_response2.json()["wallet"]["public_key"]
 
     request_data = {"from_key": public_key1, "to_key": public_key2, "amount": 100}
-    response = client.post("/transactions", json=request_data, headers=user_response.json()["user"])
+    response = client.post(
+        "/transactions", json=request_data, headers=user_response.json()["user"]
+    )
 
     assert response.status_code == 419
-    assert response.json()["error"]["message"] == f"Wallet with address <{public_key1}> does not have enough balance."
+    assert (
+        response.json()["error"]["message"]
+        == f"Wallet with address <{public_key1}> does not have enough balance."
+    )
 
 
 def test_get_transactions_success(client: TestClient) -> None:
